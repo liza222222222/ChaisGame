a0ee822ffbeb298732c87877c02ff48804d2b1e2
liza222222222 <120196276+liza222222222@users.noreply.github.com>
2023-03-20 01:40:40 +0200

    Add files via upload

---
+++ 2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+
+namespace _2
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BuildingFactory KNOKFactory1 = new KNOKFactory();
+            BuildingFactory NiledFactory1 = new NiledFactory();
+            BuildingFactory PikopFactory1 = new PikopFactory();
+            char n = ' ';
+            while (n!='4')
+            {
+                Console.WriteLine("Если хотите построить дом в классическом стиле нажмите 1");
+                Console.WriteLine("Если хотите построить дом в готическом стиле нажмите 2");
+                Console.WriteLine("Если хотите построить дом стиле китайсокм нажмите 3");
+                Console.WriteLine("Хотите выйти нажмите 4");
+                n =Convert.ToChar( Console.ReadLine());
+
+                if (n=='1')
+                {
+                    Console.Write("Строительная компания KNOK ");
+                    Building classik = new Building(KNOKFactory1);
+                    classik.Buildd();
+                    Console.WriteLine();
+
+                }
+                else if (n == '3')
+                {
+                    Console.Write("Строительная компания Niled ");
+                    Building baroque = new Building(PikopFactory1);
+                    baroque.Buildd();
+                    Console.WriteLine();
+                }
+                else if (n == '2')
+                {
+                    Console.Write("Строительная компания Pikop ");
+                    Building gothic = new Building(NiledFactory1);
+                    gothic.Buildd();
+                    Console.WriteLine();
+                   Console.Write("Мы случайно построили Notre-Dame De Paris(");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+
+            }
+        }
+    }
+}
