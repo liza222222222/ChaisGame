9cdf9db78d273068d25eafd8b52d8d390f931cc3
liza222222222 <120196276+liza222222222@users.noreply.github.com>
2023-03-21 09:52:50 +0200

    Add files via upload

---
+++ синглтон/singlton/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace singlton
+{
+    public class Singleton
+    {
+        public static Singleton Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+                return _instance;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        protected Singleton()
+        {
+        }
+
+        private static Singleton _instance = null;
+        public string[] k;
+        public void file(string s)
+        {
+            List<string> k1 = new List<string>();
+            if (s != "")
+            {
+                k1.Add(s);
+                File.AppendAllLines("E:/NANI/programmer/кпз/spy.txt", k1);
+            }
+            else
+            {
+                k = File.ReadAllLines("E:/NANI/programmer/кпз/spy.txt");
+
+            }
+        }
+
+    }
+}
